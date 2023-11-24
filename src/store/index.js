659fe0e3e94b41e357a2c4c4d22db3de8d612b80
andrey <sn05coast@gmail.com>
2023-11-24 14:47:25 +0300

    init

--- src/store/index.js
+++ src/store/index.js
@@ -204,10 +204,6 @@ const useStore = create((set, get) => ({
         }
       });
     });
-<<<<<<< HEAD
-=======
-    console.log(Array.from(new Set(newTags)));
->>>>>>> 57b958bd0d54f1f7c5a56b84930d203904709354
     set((state) => ({
       data: { ...state.data, tags: Array.from(new Set(newTags)) },
     }));
@@ -226,10 +222,6 @@ const useStore = create((set, get) => ({
           price = item.answer[0];
         }
       });
-<<<<<<< HEAD
-=======
-      console.log(price);
->>>>>>> 57b958bd0d54f1f7c5a56b84930d203904709354
       set((state) => ({
         data: { ...state.data, ipotekaPrice: price },
       }));
@@ -245,10 +237,6 @@ const useStore = create((set, get) => ({
         }
       });
     });
-<<<<<<< HEAD
-=======
-    console.log(ipotekaTags);
->>>>>>> 57b958bd0d54f1f7c5a56b84930d203904709354
     set((state) => ({
       data: { ...state.data, ipotekaTags },
     }));
